10cdfbf89a486a978ee0edfc3cab7635d5c2994f
hugo395 <hugofsrodrigues@gmail.com>
2020-03-28 12:06:51 +0000

    done

--- JS.js/highscores.js
+++ JS.js/highscores.js
@@ -3,6 +3,6 @@ const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
 
 highScoresList.innerHTML = highScores
 .map( score =>{
-return `<ul class = 'high-score> ${score.name} - ${score.score}</ul>`;
+return `<ul class = 'high-score'> ${score.name} - ${score.score}</ul>`;
 })
 .join(""); // join the array element's 
